bb3fe3b6fe88e89ba2760093a5fe30f33f61496b
masp1234 <mart2c04@stud.kea.dk>
2022-02-25 14:56:34 +0100

    initial commit

---
+++ src/main/java/com/example/guiprojekt/GuiProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.guiprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/guiprojekt/GuiProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guiprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
