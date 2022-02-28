1ca8a8634a95a61b54069a7962a9724d6719d92c
masp1234 <mart2c04@stud.kea.dk>
2022-02-28 10:18:29 +0100

    folders

---
+++ src/main/java/com/example/guiprojekt/controllers/GuiController.java
@@ -0,0 +1,16 @@
+package com.example.guiprojekt.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GuiController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+
+}
